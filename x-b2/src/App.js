6c0692303e4ee1a2a0b7f14e5ecaba01855ed20f
Albertwo512 <122117432+Albertwo512@users.noreply.github.com>
2024-05-21 13:56:36 -0600

    i connect back4App to react app
    i connect back4App to react and i make test to showed

--- x-b2/src/App.js
+++ x-b2/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Parse from 'parse/dist/parse.min.js';
+
+
+
+// Configuración de inicialización de Parse
+const app_id = process.env.REACT_APP_PARSE_APP_ID;
+const javascript_key = process.env.REACT_APP_PARSE_APP_JAVASCRIPT_KEY
+const REACT_APP_PARSE_HOST_URL = 'https://parseapi.back4app.com/parse';
+
+// Inicializar Parse
+Parse.initialize(app_id, javascript_key);
+Parse.serverURL = REACT_APP_PARSE_HOST_URL; 
+
 
 function App() {
+  const fetchAllCars = async () => {
+    const query = new Parse.Query('B4aVehicle');
+    try {
+      const allCarsGet = await query.find();
+      allCarsGet.forEach((item) => {
+        console.log(item);
+      });
+    } catch (error) {
+      console.error('Error while fetching cars: ', error);
+    }
+  };
+
+  fetchAllCars();  // Llama a la función fetchAllCars
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Prueba</h1>
       </header>
     </div>
   );
